a71298a61c6973b5b66295095f3ca204109c173d
Jason Mei <jason.mei403@gmail.com>
2018-11-08 06:14:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,6 @@
 class Student
   attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
@@ -7,5 +8,7 @@ class Student
     @name = name
     @grade = grade
   end
+
+
 
 end
